eb0823bed3faa34bca5e698b08ff6bb6c2fee163
RA <robert@andronache.net>
2025-12-17 17:32:32 +0200

    feat: Bump application version to 1.0.4

--- foundation/application.go
+++ foundation/application.go
@@ -21,7 +21,7 @@ import (
 	"github.com/samber/do/v2"
 )
 
-const Version = "1.0.3"
+const Version = "1.0.4"
 
 // Application is the main application container.
 // It orchestrates the entire framework lifecycle.
